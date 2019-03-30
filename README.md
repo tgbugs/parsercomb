1a1052e456b441d5935c7025619b6c51328b97d0
Tom Gillespie <tgbugs@gmail.com>
2019-03-30 17:15:14 -0400

    setup.py updated added travis, manifest, Pipfile, coveragerc

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # pyrser-combinator
+[![Build Status](https://travis-ci.org/tgbugs/hyputils.svg?branch=master)](https://travis-ci.org/tgbugs/hyputils)
+[![Coverage Status](https://coveralls.io/repos/github/tgbugs/hyputils/badge.svg?branch=master)](https://coveralls.io/github/tgbugs/hyputils?branch=master)
+
 python parser combinator and parsers
--- setup.py
+++ setup.py
@@ -5,20 +5,34 @@ from setuptools import setup
 with open('README.md', 'rt') as f:
     long_description = f.read()
 
+tests_require = ['pytest', 'pytest-cov']
 setup(name='pysercomb',
       version='0.0.1',
-      description='assorted',
+      description='parser combinator library and assorted parsers',
       long_description=long_description,
       long_description_content_type='text/markdown',
       url='https://github.com/tgbugs/parsercomb',
       author='Tom Gillespie',
       author_email='tgbugs@gmail.com',
       license='MIT',
-      classifiers=[],
-      keywords='python parsing parser combinator units parsec protc',
-      packages=['pysercomb'],
-      install_requires=[ ],
-      extras_require={'dev':[]},
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+      ],
+      keywords='python parsing parser combinator scientific units parsec protc',
+      packages=[
+          'pysercomb',
+          'pysercomb.parsers',
+          'pysercomb.pyr',
+      ],
+      python_requires='>=3.6',
+      tests_require=tests_require,
+      install_requires=[],
+      extras_require={'dev': [],
+                      'test': tests_require,
+                     },
       scripts=[],
       entry_points={'console_scripts': [ ],},
       data_files=[]  # TODO
